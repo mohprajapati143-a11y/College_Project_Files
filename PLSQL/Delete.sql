1bc69af638760df48c5a5cfa0401a72299b581ea
OpenCrafted <mohprajapati143@gmail.com>
2026-01-25 18:24:03 +0530

    Update Delete.sql

--- PLSQL/Delete.sql
+++ PLSQL/Delete.sql
@@ -1,16 +1,29 @@
-SET SERVEROUTPUT ON;
+CREATE TABLE employ (
+    empno NUMBER(4),
+    ename VARCHAR2(20),
+    job   VARCHAR2(20),
+    sal   NUMBER(7,2)
+);
+-------------------------------------------------------------
+INSERT INTO employ VALUES (101, 'RAM',   'CLERK', 3000);
+INSERT INTO employ VALUES (102, 'SAM',   'MANAGER', 8000);
+INSERT INTO employ VALUES (103, 'RAJ',   'CLERK', 3200);
+INSERT INTO employ VALUES (104, 'ANU',   'ANALYST', 7000);
+
+COMMIT;
+-------------------------------------------------------------
 DECLARE
     no NUMBER(3);
 BEGIN
-    DELETE FROM employ WHERE job = '&job';
+    DELETE FROM employ
+    WHERE job = '&job';
 
-    IF SQL%NOTFOUND THEN
+    IF SQL%ROWCOUNT = 0 THEN
         DBMS_OUTPUT.PUT_LINE('There is no employee doing the job');
-    ELSIF SQL%FOUND THEN
-        DBMS_OUTPUT.PUT_LINE('Some data has been retrieved by the cursor');
+    ELSE
+        no := SQL%ROWCOUNT;
+        DBMS_OUTPUT.PUT_LINE('Employee records deleted successfully');
+        DBMS_OUTPUT.PUT_LINE('No of records deleted = ' || no);
     END IF;
-
-    no := SQL%ROWCOUNT;
-    DBMS_OUTPUT.PUT_LINE('No of records deleted = ' || no);
 END;
 /
