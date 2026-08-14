325fc65e4879c146916d876b88223e629e1fe561
OpenCrafted <mohprajapati143@gmail.com>
2026-08-14 19:45:07 +0530

    new

--- JAVA/SingleInheritanceDemo.java
+++ JAVA/SingleInheritanceDemo.java
@@ -1,17 +1,31 @@
 class Rectangle {
+    public static final int height = 0;
+    public static final int x = 0;
     double length, width;
+    public int y;
+
+    Rectangle(int playerX, int playerY, int playerSize, int playerSize2) {
+        // Default constructor
+    }
 
     void getData(double l, double w) {
         length = l;
         width = w;
     }
+
+    public boolean intersects(Rectangle enemy) {
+        throw new UnsupportedOperationException("Unimplemented method 'intersects'");
+    }
 }
 
 class Area extends Rectangle {
+    Area() {
+        super(0, 0, 0, 0);
+    }
+
     double findArea() {
         return length * width;
     }
-    
 }
 
 public class SingleInheritanceDemo {
